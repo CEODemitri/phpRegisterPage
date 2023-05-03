4fcfa18cb5a4d93e2770b6a581c45a488d6419d1
CEODemitri <colorcodex8@Colors-iMac.local>
2023-05-02 23:05:18 -0500

    trying out update feature

--- index.php
+++ index.php
@@ -13,6 +13,9 @@
 		<label for="user">Name:</label><br/>
 		<input type="text" name="name" id="name" required> <br /> <br />
 
+		<label for="user">Name:</label><br/>
+		<input type="text" name="name" id="name" required> <br /> <br />
+
 		<label for="email">Email:</label><br/>
 		<input type="email" name="email" id="email required> <br /> <br />
 
